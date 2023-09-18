852baddf5124c188be83679aeeadb4164eb3af1c
HumzaHashmi <139787367+HumzaHashmi@users.noreply.github.com>
2023-09-17 22:15:33 -0400

    Update Books.java

--- project/Books.java
+++ project/Books.java
@@ -2,7 +2,7 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
-package coe528.project;
+package proj;
 
 import javafx.scene.control.CheckBox;
 
